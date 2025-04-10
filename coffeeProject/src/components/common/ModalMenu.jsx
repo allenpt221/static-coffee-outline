27400f1e46f907dadc02353e429cf15f830bc2f0
allenpt221 <allenpt26@gmail.com>
2025-04-10 19:02:00 +0800

    prevent the inner child in the onclick event

--- coffeeProject/src/components/common/ModalMenu.jsx
+++ coffeeProject/src/components/common/ModalMenu.jsx
@@ -6,12 +6,13 @@ const ModalMenu = ({ isOpen, isClose, image, title, descrp, healthBene }) => {
     if (!isOpen) return null;
 
     return (
-        <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50" onClick={isClose}>
             <motion.div 
             initial={{ opacity: 0, y: 0 }}
             animate={{ opacity: 1, y: 20 }}
             exit={{ opacity: 0, y: -100 }} 
             transition={{ duration: 0.3 }}
+            onClick={(e) => e.stopPropagation()}
             className="flex sm:flex-row flex-col gap-2 bg-white sm:p-5 p-0 rounded-lg shadow-lg sm:items-start items-end text-start sm:w-[50rem] w-[19rem] mx-2">
                 <motion.button
                     onClick={isClose}
