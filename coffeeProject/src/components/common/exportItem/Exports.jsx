f2740c51fa3727da8c0f4b198bde02948be48f42
allenpt221 <allenpt26@gmail.com>
2025-04-14 18:09:34 +0800

    make the image ice cafe to import to not make error when post it to the vercel

--- coffeeProject/src/components/common/exportItem/Exports.jsx
+++ coffeeProject/src/components/common/exportItem/Exports.jsx
@@ -1,131 +1,157 @@
+// import image for the coffees
+import Espresso from '/image/HotCoffee/Espresso.png'
+import Americano from '/image/HotCoffee/Americano.png'
+import Latte from '/image/HotCoffee/Latte.png'
+import Cappuccino from '/image/HotCoffee/Cappuccino.png'
+import FlatWhite from '/image/HotCoffee/FlatWhite.png'
+import Macchiato from '/image/HotCoffee/Macchiato.png'
+import Mocha from '/image/HotCoffee/Mocha.png'
+import Turkish from '/image/HotCoffee/Turkish.png'
+import Irish from '/image/HotCoffee/Irish.png'
+
+// import image for the ice cafe
+import IceAmericano from '/image/IceCafe/Americano.png'
+import IceLatte from '/image/IceCafe/IceLatte.png'
+import IceMocha from '/image/IceCafe/IceMocha.png'
+import IceMacchiato from '/image/IceCafe/IceMacchiato.png'
+import SweetCream from '/image/IceCafe/SweetCream.png'
+import EspressoTonic from '/image/IceCafe/EspressoTonic.png'
+import Vietnamese from '/image/IceCafe/Vietnamese.png'
+import BrownSugarShaken from '/image/IceCafe/BrownSugarShaken.png'
+import MatchaLatte from '/image/IceCafe/MatchaLatte.png'
+import DirtyMatcha from '/image/IceCafe/DirtyMatcha.png'
+
+
 // Exporting arrays of coffee items and their details for use in a React application
 export const hotcoffees = [
-    { id: 0, image: '/image/HotCoffee/Espresso.png', text: 'Espresso' },
-    { id: 1, image: '/image/HotCoffee/Americano.png', text: 'Americano' },
-    { id: 2, image: '/image/HotCoffee/Latte.png', text: 'Latte' },
-    { id: 3, image: '/image/HotCoffee/Cappuccino.png', text: 'Cappuccino' },
-    { id: 4, image: '/image/HotCoffee/FlatWhite.png', text: 'Flat White' },
-    { id: 5, image: '/image/HotCoffee/Macchiato.png', text: 'Macchiato' },
-    { id: 6, image: '/image/HotCoffee/Mocha.png', text: 'Mocha' },
-    { id: 7, image: '/image/HotCoffee/Turkish.png', text: 'Turkish Coffee' },
-    { id: 8, image: '/image/HotCoffee/Irish.png', text: 'Irish Coffee' }
+    { id: 0, image: Espresso, text: 'Espresso' },
+    { id: 1, image: Americano, text: 'Americano' },
+    { id: 2, image: Latte, text: 'Latte' },
+    { id: 3, image: Cappuccino, text: 'Cappuccino' },
+    { id: 4, image: FlatWhite, text: 'Flat White' },
+    { id: 5, image: Macchiato, text: 'Macchiato' },
+    { id: 6, image: Mocha, text: 'Mocha' },
+    { id: 7, image: Turkish, text: 'Turkish Coffee' },
+    { id: 8, image: Irish, text: 'Irish Coffee' }
 ];
 // Exporting arrays of iced coffee items and their details for use in a React application
 export const Icecoffees = [
-    { id: 0, image: '/image/IceCafe/Americano.png', text: 'Iced Americano' },
-    { id: 1, image: '/image/IceCafe/IceCaramelLatte.png', text: 'Iced Latte' },
-    { id: 2, image: '/image/IceCafe/IceMocha.png', text: 'Iced Mocha' },
-    { id: 3, image: '/image/IceCafe/IceMacchiato.png', text: 'Iced Caramel Macchiato' },
-    { id: 4, image: '/image/IceCafe/SweetCream.png', text: 'Iced Coffee With Sweet cream' },
-    { id: 5, image: '/image/IceCafe/EspressoTonic.png', text: 'Iced Espresso Tonic' },
-    { id: 6, image: '/image/IceCafe/Vietnamese.png', text: 'Vietnamese Iced Coffee' },
-    { id: 7, image: '/image/IceCafe/BrownSugarShaken.png', text: 'Iced Brown Sugar Shaken Espresso' },
-    { id: 8, image: '/image/IceCafe/MatchaLatte.png', text: 'Iced Matcha Latte' },
-    { id: 9, image: '/image/IceCafe/DirtyMatcha.png', text: 'Iced Matcha Espresso Fusion' }
+    { id: 0, image: IceAmericano, text: 'Iced Americano' },
+    { id: 1, image: IceLatte, text: 'Iced Latte' },
+    { id: 2, image: IceMocha, text: 'Iced Mocha' },
+    { id: 3, image: IceMacchiato, text: 'Iced Caramel Macchiato' },
+    { id: 4, image: SweetCream, text: 'Iced Coffee With Sweet cream' },
+    { id: 5, image: EspressoTonic, text: 'Iced Espresso Tonic' },
+    { id: 6, image: Vietnamese, text: 'Vietnamese Iced Coffee' },
+    { id: 7, image: BrownSugarShaken, text: 'Iced Brown Sugar Shaken Espresso' },
+    { id: 8, image: MatchaLatte, text: 'Iced Matcha Latte' },
+    { id: 9, image: DirtyMatcha, text: 'Iced Matcha Espresso Fusion' }
 ];
 
 // exporting arrays of Hotcoffee items and their details for use in the modal
 export const ModalMenus = [
     {
-        image: '/image/HotCoffee/Espresso.png',
+        image: Espresso,
         title: 'Espresso',
         descrp: 'Espresso is a concentrated coffee beverage brewed by forcing hot water through finely-ground coffee beans. It serves as the base for many coffee drinks.',
         healthBene: 'Strong, Bold, Energizing'
     },
     {
-        image: '/image/HotCoffee/Americano.png',
+        image: Americano,
         title: 'Americano',
         descrp: 'Americano is a coffee drink made by diluting espresso with hot water, resulting in a similar strength to brewed coffee but with a different flavor profile.',
         healthBene: 'Rich, Smooth, Bold'
     },
     {
-        image: '/image/HotCoffee/Latte.png',
+        image: Latte,
         title: 'Latte',
         descrp: 'Latte is a coffee drink made with espresso and steamed milk, topped with a small amount of milk foam. It has a creamy texture and is often flavored with syrups.',
         healthBene: 'Creamy, Velvety, Smooth'
     },
     {
-        image: '/image/HotCoffee/Cappuccino.png',
+        image: Cappuccino,
         title: 'Cappuccino',
         descrp: 'Cappuccino is made with espresso, hot milk, and a layer of foamed milk, creating a creamy texture and a balance between the strength of coffee and milk.',
         healthBene: 'Frothy, Balanced, Creamy'
     },{
-        image: '/image/HotCoffee/FlatWhite.png',
+        image: FlatWhite,
         title: 'Flat White',
         descrp: 'Flat White is a coffee drink made with espresso and microfoam (steamed milk with small, fine bubbles), resulting in a velvety texture and strong coffee flavor.',
         healthBene: 'Smooth, Creamy, Rich'
 
     },{
-        image: '/image/HotCoffee/Macchiato.png',
+        image: Macchiato,
         title: 'Macchiato',
         descrp: 'Macchiato is an espresso-based drink with a small amount of steamed milk or foam, allowing the espresso flavor to shine through.',
         healthBene: 'Velvety, Smooth, Creamy'
     },{
-        image: '/image/HotCoffee/Mocha.png',
+        image: Mocha,
         title: 'Mocha',
         descrp: 'Mocha is a chocolate-flavored variant of a latte, made with espresso, steamed milk, and chocolate syrup or cocoa powder.',
         healthBene: 'Bold, Strong, Layered'
     },{
-        image: '/image/HotCoffee/Turkish.png',
+        image: Turkish,
         title: 'Turkish Coffee',
         descrp: 'Turkish Coffee is a strong, unfiltered brew made in a small pot called a cezve, using very finely ground coffee. It has a thick, rich texture and bold flavor, often served sweet and in small cups. Traditionally enjoyed slowly, it’s both a cultural ritual and a symbol of hospitality—sometimes even followed by fortune-telling from the coffee grounds.',
         healthBene: 'Rich, Spicy, Intense'
     },{
-        image: '/image/HotCoffee/Irish.png',
+        image: Irish,
         title: 'Irish Coffee',
         descrp: 'Irish Coffee is a cocktail made with hot coffee, Irish whiskey, sugar, and topped with cream. It is often enjoyed as a dessert drink.',
         healthBene: 'Warm, Spiked, Smooth'
     }
 ];
+
+
 // exporting arrays of Icecoffee items and their details for use in the modal
 export const ModalMenusIce = [
     {
-        image: '/image/IceCafe/Americano.png',
+        image: IceAmericano,
         title: 'Iced Americano',
         descrp: 'Iced Americano is a refreshing coffee drink made by diluting espresso with cold water and ice, resulting in a smooth and bold flavor.',
         healthBene: 'Refreshing, Bold, Smooth'
     },{
-        image: '/image/IceCafe/IceCaramelLatte.png',
+        image: IceLatte,
         title: 'Iced Latte',
         descrp: 'Iced Latte is a cold coffee drink made with espresso and chilled milk, served over ice. It has a creamy texture and can be flavored with syrups.',
         healthBene: 'Creamy, Smooth, Refreshing'
     },{
-        image: '/image/IceCafe/IceMocha.png',
+        image: IceMocha,
         title: 'Iced Mocha',
         descrp: 'Iced Mocha is a cold coffee drink made with espresso, chocolate syrup, and milk, served over ice. It has a rich chocolate flavor and is often topped with whipped cream.',
         healthBene: 'Rich, Creamy, Sweet'
     },{
-        image: '/image/IceCafe/IceMacchiato.png',
+        image: IceMacchiato,
         title: 'Iced Caramel Macchiato',
         descrp: 'Iced Caramel Macchiato is a cold coffee drink made with espresso, milk, and caramel syrup, served over ice. It has a sweet and creamy flavor.',
         healthBene: 'Sweet, Creamy, Refreshing'
     }, {
-        image: '/image/IceCafe/SweetCream.png',
+        image: SweetCream,
         title: 'Iced Coffee With Sweet cream',
         descrp: 'Iced Coffee with Sweet Cream is a cold coffee drink made with brewed coffee, ice, and sweet cream. It has a rich and creamy flavor.',
         healthBene: 'Rich, Creamy, Sweet'
     }, {
-        image: '/image/IceCafe/EspressoTonic.png',
+        image: EspressoTonic,
         title: 'Iced Espresso Tonic',
         descrp: 'Iced Espresso Tonic is a refreshing coffee drink made with espresso, tonic water, and ice. It has a unique flavor profile that combines the bitterness of coffee with the effervescence of tonic.',
         healthBene: 'Refreshing, Unique, Bold'
     }, {
-        image: '/image/IceCafe/Vietnamese.png',
+        image: Vietnamese,
         title: 'Vietnamese Iced Coffee',
         descrp: 'Vietnamese Iced Coffee is a strong coffee drink made with dark roast coffee, sweetened condensed milk, and ice. It has a rich and sweet flavor.',
         healthBene: 'Rich, Sweet, Bold'
     },{
-        image : '/image/IceCafe/BrownSugarShaken.png',
+        image : BrownSugarShaken,
         title: 'Iced Brown Sugar Shaken Espresso',
         descrp: 'Iced Brown Sugar Shaken Espresso is a cold coffee drink made with espresso, brown sugar, and ice, shaken to create a frothy texture.',
         healthBene: 'Sweet, Bold, Refreshing'
     },{
-        image: '/image/IceCafe/MatchaLatte.png',
+        image: MatchaLatte,
         title: 'Iced Matcha Latte',
         descrp: 'Iced Matcha Latte is a cold drink made with matcha green tea powder, milk, and ice. It has a unique flavor and is often sweetened.',
         healthBene: 'Unique, Creamy, Refreshing'
     }, {
-        image: '/image/IceCafe/DirtyMatcha.png',
+        image: DirtyMatcha,
         title: 'Iced Matcha Espresso Fusion',
         descrp: 'Iced Matcha Espresso Fusion is a cold drink made with matcha green tea powder, espresso, milk, and ice. It has a unique flavor and is often sweetened.',
         healthBene: 'Unique, Creamy, Refreshing'
