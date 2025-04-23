4b8aad4df4ebaac9998f28f506eb19ed1fd8c7f4
Allenpt221 <Allenpt26@gmail.com>
2025-04-24 04:42:04 +0800

    make import the image to not cause error img src out

--- coffeeProject/src/components/pages/Home.jsx
+++ coffeeProject/src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 
-import SlideShow from '../common/SlideShow'
+import SlideShow from '../common/SlideShow';
 
-
+import Quality from '/image/QualityBeans.png';
+import Measure from '/image/MeasureCoffee.png';
+import BrewTime from '/image/BrewTime.png';
 
 // import icons
 
@@ -18,21 +20,21 @@ const Home = () => {
 
   const coffeeSection = [{
     title: 'Use Fresh, Quality Beans',
-    image: '/image/QualityBeans.png',
+    image: Quality,
     text: 'The foundation of great coffee is using high-quality, freshly roasted beans. Try to purchase beans that have been roasted within the last two weeks and grind them just before brewing. Freshness is key to getting the best flavors.',
     buttonText: 'Quality Beans',
     href: 'https://www.amazon.com/s?k=coffee+beans&crid=3RTKQFWI1YXFF&sprefix=coffee+bean%2Caps%2C338&ref=nb_sb_noss_1',
     altertext: 'error Image'
   }, {
     title: 'Measure Your Coffee and Water',
-    image: '/image/MeasureCoffee.png',
+    image: Measure,
     text: 'The appropriate coffee-to-water ratio varies depending on your brewing method, but a good rule of thumb is about 1:15 (1 gram of coffee for every 15 grams of water). A kitchen scale can help you achieve the proper balance and avoid under- or over-extraction.',
     buttonText: 'Measurement',
     href: 'https://www.foodnetwork.com/recipes/articles/coffee-water-ratio',
     altertext: 'error Image'
   },{
     title: 'Brew Time & Method',
-    image: '/image/BrewTime.png',
+    image: BrewTime,
     text: 'The time water stays in contact with coffee impacts its flavor—around 4 minutes for French press, 2-4 minutes for pour-over, and 25-30 seconds for espresso. Adjusting brew time helps you achieve the perfect taste!',
     buttonText: 'Brewing Method',
     href: 'https://podiumcoffeeclub.com/blogs/blog/brewing-time-for-different-coffee-brewing-methods?srsltid=AfmBOooYpTiwGo5JmqfCDxe4gpN5kmI1jVVIO8fNuS2slRWKlaahx8J6',
