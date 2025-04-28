4875b41ebfee0cef728c3b22faf0c3955f7335d6
Allenpt221 <Allenpt26@gmail.com>
2025-04-28 18:00:40 +0800

    add loading state to the image when the network is slow

--- coffeeProject/src/components/pages/ProductMenu.jsx
+++ coffeeProject/src/components/pages/ProductMenu.jsx
@@ -13,7 +13,11 @@ const ProductMenu = () => {
     const [isOpenModalPastry, SetisOpenModalPastry] = useState(null);
 
     
+    const [loadingImage, setLoadingImage] = useState({});
 
+    const handleImageLoad = (id) => {
+        setLoadingImage((prevState) => ({ ...prevState, [id]: true }));
+    }
 
     return (
         <motion.section
@@ -23,6 +27,7 @@ const ProductMenu = () => {
             transition={{ duration: 0.5 }}>
             <div className='p-5 font-noto'>
                 <h1 className='font-semibold text-[1.4rem]'>Brewed</h1>
+                
                 {/* hot coffee section */}
                 <div className='lg:max-w-[90rem] md:max-w-[45rem] sm:max-w-[85rem] max-w-[20rem] m-auto'>
                     <h1 className='font-semibold text-[1.2rem] mt-5 mx-5'>Morning Roast</h1>
@@ -31,13 +36,23 @@ const ProductMenu = () => {
                         <div className='grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-4 mt-5 text-center'>
                             {hotcoffees.map((item) => (
                                 <motion.div 
-                                    key={item.id}
+                                    key={item.id}  // Use a unique key for each item
                                     onClick={() => SetisOpenModalHot(item.id)}  // Pass the function reference with the correct index
                                     whileHover={{ scale: 1.02 }}
                                     whileTap={{ scale: 0.99 }}
                                     className='shadow-lg bg-[#9e9e9e25] rounded-md p-4'>
-                                    <img src={item.image} alt="Error Morning Roast" className='w-[20rem] h-[8rem] m-auto mb-2' />
+                                    {!loadingImage[item.id] && (
+                                    <div className="flex items-center justify-center h-[150px] w-full  animate-pulse rounded">
+                                        <span className="loading loading-spinner loading-xl"></span>
+                                    </div>
+                                    )}
+                                    <div className={` ${loadingImage[item.id] ? 'block' : 'hidden'}`}>
+                                    {/* Show the image only when it's loaded */}
+                                    <img
+                                    onLoad={() => handleImageLoad(item.id)}
+                                    src={item.image} alt="Error Morning Roast" className='w-[20rem] h-[8rem] m-auto mb-2' />
                                     <h1 className='font-medium text-sm text-black/80'>{item.text}</h1>
+                                    </div>
                                 </motion.div>
                             ))}
                         </div>
@@ -50,13 +65,23 @@ const ProductMenu = () => {
                         <div className='grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-4 mt-5 text-center'>
                             {Icecoffees.map((items) => (
                                 <motion.div 
-                                    key={items}
+                                    key={items.id}
                                     onClick={() => SetisOpenModalIce(items.id)}
                                     whileHover={{ scale: 1.02 }}
                                     whileTap={{ scale: 0.99 }}
                                     className='shadow-lg bg-[#9e9e9e25] rounded-md p-4'>
-                                    <img src={items.image} alt="Error Chilled Coffee" className='w-[20rem] h-[8rem] m-auto mb-2' />
+                                    {!loadingImage[items.id] && (
+                                    <div className="flex items-center justify-center h-[150px] w-full  animate-pulse rounded">
+                                        <span className="loading loading-spinner loading-xl"></span>
+                                    </div>
+                                    )}
+                                    <div className={` ${loadingImage[items.id] ? 'block' : 'hidden'}`}>
+                                    {/* Show the image only when it's loaded */}
+                                    <img
+                                    onLoad={() => handleImageLoad(items.id)}
+                                    src={items.image} alt="Error Chilled Coffe" className='w-[20rem] h-[8rem] m-auto mb-2' />
                                     <h1 className='font-medium text-sm text-black/80'>{items.text}</h1>
+                                    </div>
                                 </motion.div>
                             ))}
                         </div>
@@ -75,8 +100,18 @@ const ProductMenu = () => {
                                     whileHover={{ scale: 1.02 }}
                                     whileTap={{ scale: 0.99 }}
                                     className='shadow-lg bg-[#9e9e9e25] rounded-md p-4'>
-                                    <img src={Pastry.image} alt="Error Pastry" className='w-[20rem] h-[8rem] m-auto mb-2' />
+                                    {!loadingImage[Pastry.id] && (
+                                    <div className="flex items-center justify-center h-[150px] w-full  animate-pulse rounded">
+                                        <span className="loading loading-spinner loading-xl"></span>
+                                    </div>
+                                    )}
+                                    <div className={` ${loadingImage[Pastry.id] ? 'block' : 'hidden'}`}>
+                                    {/* Show the image only when it's loaded */}
+                                    <img
+                                    onLoad={() => handleImageLoad(items.id)}
+                                    src={Pastry.image} alt="Error Pastry" className='w-[20rem] h-[8rem] m-auto mb-2' />
                                     <h1 className='font-medium text-sm text-black/80'>{Pastry.text}</h1>
+                                    </div>
                                 </motion.div>
                             ))}
                         </div>
